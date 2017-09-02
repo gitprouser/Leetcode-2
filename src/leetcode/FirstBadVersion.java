c31f3054c4c876b8ab890dc12068a65f4b763d1a
shi-edward <shi_edward@icloud.com>
2017-09-02 15:14:54 -0500

    9.2

--- src/leetcode/FirstBadVersion.java
+++ src/leetcode/FirstBadVersion.java
@@ -29,7 +29,18 @@ public class FirstBadVersion {
      * @return
      */
     public int firstBadVersion(int n) {
-
+        int start = 1;
+        int end = n;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (isBadVersion(mid)) {
+                end = mid;
+            } else start = mid;
+        }
+        if (isBadVersion(start)) {
+            return start;
+        }
+        return end;
     }
 
     public boolean isBadVersion(int mid) {
--- src/leetcode/IntersectionofTwoArrays.java
+++ src/leetcode/IntersectionofTwoArrays.java
@@ -1,7 +1,9 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Edward on 28/07/2017.
@@ -25,21 +27,91 @@ public class IntersectionofTwoArrays {
      * @param nums2
      * @return
      */
+
+    // binary search time : O(nlogn) space : O(n)
     public static int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> set = new HashSet<>();
-        ArrayList<Integer> arr = new ArrayList<>();
-        for (int num : nums1) {
-            set.add(num);
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return new int[]{};
         }
-        for (int num : nums2) {
-            if (set.contains(num)) {
-                arr.add(num);
-                set.remove(num);
+        HashSet<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);
+        for (Integer num : nums1) {
+            if (binarySearch(nums2, num)) {
+                set.add(num);
             }
         }
         int k = 0;
-        int[] res = new int[arr.size()];
-        for (int num : arr) {
+        int[] res = new int[set.size()];
+        for (Integer num : set) {
+            res[k++] = num;
+        }
+        return res;
+    }
+
+    public static boolean binarySearch(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] > target) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        if (nums[start] == target || nums[end] == target) return true;
+        return false;
+    }
+
+    // Arrays.sort time : O(nlogn) space : O(n);
+    public static int[] intersection2(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return new int[]{};
+        }
+        HashSet<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int k = 0;
+        int[] res = new int[set.size()];
+        for (Integer num : set) {
+            res[k++] = num;
+        }
+        return res;
+    }
+
+    // time : O(n) space : O(n);
+    public static int[] intersection3(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return new int[]{};
+        }
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> ret = new HashSet<>();
+        for (Integer num : nums1) {
+            set.add(num);
+        }
+        for (Integer num : nums2) {
+            if (set.contains(num)) {
+                ret.add(num);
+            }
+        }
+        int k = 0;
+        int[] res = new int[ret.size()];
+        for (Integer num : ret) {
             res[k++] = num;
         }
         return res;
