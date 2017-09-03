34c1d62dbe79bd9fe7cdee693b8f2814d6060e4d
shi-edward <shi_edward@icloud.com>
2017-09-03 12:44:43 -0500

    9.3

--- src/leetcode/AAA.java
+++ src/leetcode/AAA.java
@@ -12,9 +12,19 @@ import java.util.*;
  */
 public class AAA {
     // Encodes a tree to a single string.
-
+    public int res(int N, int K, int L) {
+        long res = 1, r = 1000000007;
+        for (int i = 0; i < K; i++) {
+            res *= (N - i);
+        }
+        res *= (long)Math.pow((N-K),(L-K));
+        res = res % r;
+        return (int)res;
+    }
 
     public static void main(String[] args) {
-
+        long r = 1;
+        int L = 3, N = 3, K= 3;
+        int res = (int)Math.pow((N-K),(L-K));
     }
 }
---
+++ src/leetcode/WordBreak.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * Project Name : Leetcode
+ * Package Name : leetcode
+ * File Name : WordBreak
+ * Creator : Edward
+ * Date : Sep, 2017
+ * Description : TODO
+ */
+public class WordBreak {
+
+    /**
+     * 139. Word Break
+     * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words,
+     * determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+     * You may assume the dictionary does not contain duplicate words.
+
+     For example, given
+     s = "leetcode",
+     dict = ["leet", "code"].
+
+     Return true because "leetcode" can be segmented as "leet code".
+
+     time : O(n^2);
+     space : O(n);
+     * @param s
+     * @param wordDict
+     * @return
+     */
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+    }
+}
